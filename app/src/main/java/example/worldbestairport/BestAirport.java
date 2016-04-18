4f7badd7eced0f8e7393ada85d9be8dfa9cf5518
Lily Yung <ymll@live.hk>
2016-04-19 00:18:12 +0900

    Change to Airport

--- app/src/main/java/example/worldbestairport/BestAirport.java
+++ app/src/main/java/example/worldbestairport/BestAirport.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 public class BestAirport extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.bestairportlayout, container, false);
         return rootView;
 
     }
