d8c580d33053478435d61badfe1e764c49ff450e
Lily Yung <ymll@live.hk>
2016-04-20 01:35:08 +0900

    Add coming soon view

--- app/src/main/java/example/worldbestairport/BestAirline.java
+++ app/src/main/java/example/worldbestairport/BestAirline.java
@@ -1,11 +1,14 @@
 package example.worldbestairport;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 /**
@@ -15,16 +18,33 @@ public class BestAirline extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.bestairlinelayout, container, false);
-        ((MainActivity) getActivity()).setActionBarTitle(getString(R.string.title_section3));
 
-        Button btn2016 = (Button)rootView.findViewById(R.id.btn2016);
-        Button btn2015 = (Button)rootView.findViewById(R.id.btn2015);
-        Button btn2014 = (Button)rootView.findViewById(R.id.btn2014);
+        Button btn2016 = (Button) rootView.findViewById(R.id.btn2016);
+        Button btn2015 = (Button) rootView.findViewById(R.id.btn2015);
+        Button btn2014 = (Button) rootView.findViewById(R.id.btn2014);
+        TextView txtComingSoon = (TextView) rootView.findViewById(R.id.txt_coming_soon);
 
+        setOnClickListenerForComingYearButton(btn2016, rootView);
         setOnClickListenerForYearButton(btn2015, rootView, R.array.airlines2015);
         setOnClickListenerForYearButton(btn2014, rootView, R.array.airlines2014);
 
+        ((MainActivity) getActivity()).setActionBarTitle(getString(R.string.title_section3));
+        btn2016.performClick();
+
         return rootView;
+    }
+
+    private void setOnClickListenerForComingYearButton(Button button, final View rootView) {
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                LinearLayout contentLayout = (LinearLayout) rootView.findViewById(R.id.layout_content);
+                TextView txtComingSoon = (TextView) rootView.findViewById(R.id.txt_coming_soon);
+
+                txtComingSoon.setVisibility(View.VISIBLE);
+                contentLayout.setVisibility(View.GONE);
+            }
+        });
     }
 
     private void setOnClickListenerForYearButton(Button button, final View rootView, final int dataResourceId) {
@@ -32,6 +52,9 @@ public class BestAirline extends Fragment {
             @Override
             public void onClick(View v) {
                 String[] data = getResources().getStringArray(dataResourceId);
+                ScrollView scrollView = (ScrollView)rootView.findViewById(R.id.scroll_view);
+                LinearLayout contentLayout = (LinearLayout) rootView.findViewById(R.id.layout_content);
+                TextView txtComingSoon = (TextView) rootView.findViewById(R.id.txt_coming_soon);
 
                 TextView[] textViewGroup = new TextView[]{
                     (TextView)rootView.findViewById(R.id.first_item_name),
@@ -45,6 +68,10 @@ public class BestAirline extends Fragment {
                 for (int i=0; i<data.length && i<textViewGroup.length; i++) {
                     textViewGroup[i].setText(data[i]);
                 }
+
+                txtComingSoon.setVisibility(View.GONE);
+                contentLayout.setVisibility(View.VISIBLE);
+                scrollView.setScrollY(0);
             }
         });
     }
